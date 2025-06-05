50d4833ffcdce8f73022f975160242b093883b9e
RANJEET DESHMUKH <140232489+RANJEET1106@users.noreply.github.com>
2025-06-06 03:11:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/RANJEET1106/linux-tools.git ~/.tools
 chmod -R +x ~/.tools/Scripts
 
-grep -qxF 'export PATH="$HOME/.tools:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.tools:$PATH"' >> ~/.bashrc
-grep -qxF 'export PATH="$HOME/.tools:$PATH"' ~/.zshrc || echo 'export PATH="$HOME/.tools:$PATH"' >> ~/.zshrc
+grep -qxF 'export PATH="$HOME/.tools/Scripts:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.tools/Scripts:$PATH"' >> ~/.bashrc
+grep -qxF 'export PATH="$HOME/.tools/Scripts:$PATH"' ~/.zshrc || echo 'export PATH="$HOME/.tools/Scripts:$PATH"' >> ~/.zshrc
 
 
